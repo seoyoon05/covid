5436481aadceedfe51181086783a622b0d7c6cc8
seoyoon05 <dbstj182@gmail.com>
2023-03-31 08:43:25 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,75 @@
+(() => {
+
+    const actions = {
+        birdFlies(key) {
+            if (key) {
+            document.querySelector('[data-index="2"] .bird').style.transform = `translateX(${window.innerWidth}px, ${-window.innerHeight * 0.7}px)`;
+            } else {
+                document.querySelector('[data-index="2"] .bird').style.transform = 'translateX(-100%)';
+            }
+        },
+        birdFlies2(key) {
+            if (key) {
+            document.querySelector('[data-index="5"] .bird').style.transform = `translateX(${window.innerWidth}px)`;
+            } else {
+                document.querySelector('[data-index="5"] .bird').style.transform = 'translateX(-100%)';
+            }
+        }
+    };
+
+    const stepElems = document.querySelectorAll('.step');
+    const graphicElems = document.querySelectorAll('.graphic-item');
+    let currentItem = graphicElems[0];
+    let ioIndex;
+
+    const io = new IntersectionObserver((entries, observer) => {
+        ioIndex = entries[0].target.dataset.index;
+    });
+
+    for (let i = 0; i < stepElems.length; i++) {
+        io.observe(stepElems[i]);
+        // stepElems[i].setAttribute('data-index', i);
+        stepElems[i].dataset.index = i;
+        graphicElems[i].dataset.index = i;
+    }
+
+    function activate(action) {
+        currentItem.classList.add('visible');
+        if(action) {
+            actions[action](true);
+        }
+    }
+
+    function inactivate(action) {
+        currentItem.classList.remove('visible');
+        if(action) {
+            actions[action](false);
+        }
+    }
+
+    window.addEventListener('scroll', () => {
+        let step;
+        let boundingRect;
+
+        //for (let i = 0; i < stepElems.length; i++) {
+        for (let i = ioIndex; i < ioIndex + 2; i++) {
+            step = stepElems[i];
+            if(!step) continue;
+            boundingRect = step.getBoundingClientRect;
+            if (boundingRect.top > window.innerHeight * 0.1 &&
+                boundingRect.top < window.innerHeight * 0.8) {               
+                    inactivate(currentItem.dataset.action);                                   
+                    currentItem = graphicElems[step.dataset.index];
+                    activate(currentItem.dataset.action);
+            }
+        }
+    });
+
+    window.addEventListener('Load', () => {
+        setTimeout(() =>scrollTo(0, 0), 100);
+    });
+
+    activate();
+
+    
+}) ();
